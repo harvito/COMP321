056dc9b7e8c07f30dc9584e33ebff8f03d6e6d6c
mharve17 <mharve17@025BARESI.campus.MCGILL.CA>
2018-10-03 20:09:14 -0400

    Passes 1/4 test cases. Opted to use BigDecimal.

--- COMP321/src/a3/AllAboutThatBase.java
+++ COMP321/src/a3/AllAboutThatBase.java
@@ -3,85 +3,93 @@ package a3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class AllAboutThatBase {
-
-	private static int[] add(int[] a, int[] b, int base) {
-		//TODO: implement dis
-		return new int[0];
-	}
-	private static int[] subtract(int[] a, int[] b, int base) {
-		//TODO: implement dis
-		return new int[0];
-	}
-	private static int[] multiply(int[] a, int[] b, int base) {
-		//TODO: implement dis
-		return new int[0];
-	}
-	private static int[] divide(int[] a, int[] b, int base) {
-		//TODO: implement dis
-		return new int[0];
-	}
 	
-	private static int[] stringToIntArray(String s) {
-		int[] result = new int[s.length()];
-		char digit;
+	final public static String BASES = "0123456789abcdefghijklmnopqrstuvwxyz0";
+	
+	private static int lowestBase(String s) {
+		char greatestChar = 0;
 		for (int i = 0; i < s.length(); i++) {
-			digit = s.charAt(i);
-			if (digit >= 'a')
-				result[i] = digit - 'a' + 10;
-			else
-				result[i] = digit - '0';
+			if (s.charAt(i) > greatestChar)
+				greatestChar = s.charAt(i);
 		}
-		return result;
+		if (greatestChar == '1')
+			return 1;
+		else if (greatestChar == 'z')
+			return 36;
+        return BASES.indexOf(greatestChar+1);
 	}
 	
-	private static char intToBaseChar(int baseInt) {
-		if (baseInt == 36)
-			return '0';
-		else if (baseInt < 10)
-			return (char) (baseInt + '0');
-		else
-			return (char) (baseInt - 11 + 'a');
+	private static String check(String X, String Y, char op, String Z, int b) {
+		BigDecimal x;
+		BigDecimal y;
+		BigDecimal z;
+		
+		//special case: input is unary and uses symbol "1" instead of "0"
+		if (b == 1) {
+			// Although unspecified in the problem, assume "0" represents zero in unary
+			x = (X == "0") ? BigDecimal.ZERO : new BigDecimal(Integer.toString(X.length(), 10));
+			y = (Y == "0") ? BigDecimal.ZERO : new BigDecimal(Integer.toString(Y.length(), 10));
+			z = (Z == "0") ? BigDecimal.ZERO : new BigDecimal(Integer.toString(Z.length(), 10));
+		}
+		else {
+			x = new BigDecimal(new BigInteger(X, b));
+			y = new BigDecimal(new BigInteger(Y, b));
+			z = new BigDecimal(new BigInteger(Z, b));
+		}
+		
+		switch (op) {
+		case '+' :
+			if ((x.add(y)).equals(z))
+				return Character.toString(BASES.charAt(b));
+			break;
+		case '-' :
+			if ((x.subtract(y)).equals(z))
+				return Character.toString(BASES.charAt(b));
+			break;
+		case '*' :
+			if ((x.multiply(y)).equals(z))
+				return Character.toString(BASES.charAt(b));
+			break;
+		case '/' :
+			if ((x.divide(y)).equals(z))
+				return Character.toString(BASES.charAt(b));
+			break;
+		}
+		return "";
 	}
 
+	//using Java's BigInteger, we still need to deal with special case unary
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//System.out.println(new BigDecimal(111));
+		//System.exit(0);
 		
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine()), lowestBase;
 		char op;
 		String[] nugs;
-		int[] X, Y, Z;
+		String X, Y, Z, output;
 		for (int i = 0; i < N; i++) {
 			//find the biggest digit (0 < 9 < a < z) and start testing from there
 			//test all bases, outputting the ones that work as we go ("0" is base 36)
 			//if no bases work print "invalid"
 			nugs = br.readLine().split(" ");
-			X = stringToIntArray(nugs[0]);
-			Y = stringToIntArray(nugs[2]);
-			Z = stringToIntArray(nugs[4]);
+			X = nugs[0];
+			Y = nugs[2];
+			Z = nugs[4];
+			output = "";
+			lowestBase = lowestBase(X+Y+Z);
 			op = nugs[1].charAt(0);
-			for (int j = 1; j <= 36; j++) {
-				switch (op) {
-				case '+' :
-					if (add(X, Y, j) == Z)
-						System.out.print(intToBaseChar(j));
-					break;
-				case '-' :
-					if (subtract(X, Y, j) == Z)
-						System.out.print(intToBaseChar(j));
-					break;
-				case '*' :
-					if (multiply(X, Y, j) == Z)
-						System.out.print(intToBaseChar(j));
-					break;
-				case '/' :
-					if (divide(X, Y, j) == Z)
-						System.out.print(intToBaseChar(j));
-					break;
-				}
+			for (int j = lowestBase; j <= 36; j++) {
+				output += check(X, Y, op, Z, j);
 			}
-			
+			if (output.isEmpty())
+				System.out.println("invalid");
+			else
+				System.out.println(output);
 		}
 	}
 
