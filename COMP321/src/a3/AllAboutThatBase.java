b2fd81db56c3fd4c2ec5394b0f67671780e78d10
harvito <michael.harvey@mail.mcgill.ca>
2018-10-07 15:36:33 -0400

    Accepted by Kattis. Unary nuace discovered and addressed.

--- COMP321/src/a3/AllAboutThatBase.java
+++ COMP321/src/a3/AllAboutThatBase.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class AllAboutThatBase {
-	
+	//useful constant
 	final public static String BASES = "0123456789abcdefghijklmnopqrstuvwxyz0";
 	
+	//check for the highest symbol that occurs and output the lowest possible base
 	private static int lowestBase(String s) {
 		int res = 0;
 		Boolean isUnary = true;
@@ -21,13 +22,14 @@ public class AllAboutThatBase {
 				res = Character.digit(s.charAt(i), 36);
 			}
 		}
-		if (res == 1 && isUnary) {
+		if (res == 1 && isUnary) { //111 is unary but 101 is not
 			return 1;
 		} else {
 			return res + 1;
 		}
 	}
 	
+	//interpret s as base b and output the corresponding long value
 	private static long longFromBase(String s, int b) {
 		if (b == 1) {
 			return s.length();
@@ -39,6 +41,7 @@ public class AllAboutThatBase {
 		return res;
 	}
 	
+	//check the math operation
 	private static Boolean check(long x, char op, long y, long z) {
 		switch (op) {
 		case '+' :
@@ -49,7 +52,7 @@ public class AllAboutThatBase {
 			return x * y == z;
 		case '/' :
 			try {
-				return x%y == 0 && x/y == z;
+				return x%y == 0 && x/y == z; //integer division check
 			} catch (ArithmeticException e) {
 				return false;
 			}
@@ -61,9 +64,9 @@ public class AllAboutThatBase {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         
-        String[] ln;
-        String out;
-        int lowestBase;
+        String[] ln; //input line
+        String out; //build the output for each test case
+        int lowestBase; //start from the lowest base and work our way up
         long x, y, z;
         for (int i = 0; i < N; i++) {
         	out = "";
@@ -78,7 +81,7 @@ public class AllAboutThatBase {
         			out += BASES.charAt(b);
         		}
         	}
-        	if (out == "") {
+        	if (out == "") {//valid in no base
         		System.out.println("invalid");
         	} else {
         		System.out.println(out);
